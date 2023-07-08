687f87b152b49a465c267c0f8677179a1a54e30b
Shubh Khoda <shubhkhoda19@gmail.com>
2023-07-08 18:37:47 +0530

    Adding All Files

--- Duplicate_Num.cpp
+++ Duplicate_Num.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+    int findDuplicate(vector<int>& arr) {
         int xor1 = 0;
         int xor2 = 0;
         int ans = 0;
-        for(int i = 1; i<nums.size(); i++) {
-            xor1 = xor1 ^ i;
+        for(int i = 1; i<arr.size(); i++) {
+            xor1 = xor1 ^ i; // Xoring with 1 to n so xor becomes 1 to n
         }
 
-        for(int i = 0; i< nums.size(); i++) {
-             xor2 = xor2 ^ nums[i];
+        for(int i = 0; i< arr.size(); i++) {
+             xor2 = xor2 ^ arr[i];
              ans = xor1 ^ xor2;
         }
         return ans;
